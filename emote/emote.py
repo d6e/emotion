cf457a8ba688b33748bb03baa5a77d9b4e638e9d
Danielle Jenkins <djenkins@splunk.com>
2016-02-17 20:17:43 -0800

    Add partially implemented list option.

--- emote/emote.py
+++ emote/emote.py
@@ -8,16 +8,27 @@ import argparse
 with open("mapping.json") as f:
     emotes = json.load(f)
 
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser(
             description=sys.modules[__name__].__doc__,
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-p','--web_port')
-    args = parser.parse_args()
+    parser.add_argument('-l','--list', action="store_true",
+                        help="List all available emotes.")
 
     # Print help if no cli args are specified.
     if len(sys.argv) < 2:
         parser.print_help()
+    return parser.parse_args()
+
+def list_emotes():
+    print [e for e in emotes.keys()]
+    print [e for e in emotes.values()]
+
+def main():
+    args = parse_arguments()
+    if args.list:
+        list_emotes()
+
 
 if __name__ == "__main__":
     main()
