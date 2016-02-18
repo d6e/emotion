397b12948b3cd87953b7d1925db50224e54a545a
Danielle Jenkins <djenkins@splunk.com>
2016-02-17 20:07:25 -0800

    Add argparsing to emote module.

--- emote/emote.py
+++ emote/emote.py
@@ -1,9 +1,19 @@
+""" A simple CLI tool for quickly copying common emoticon/emoji to your
+clipboard. """
 import pyperclip
 import json
+import sys
 import argparse
 
-with open("emote_dict.json") as f:
+with open("mapping.json") as f:
     emotes = json.load(f)
 
-def parse_cli():
-    pass
+def main():
+    parser = argparse.ArgumentParser(
+            description=sys.modules[__name__].__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-p','--web_port')
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
