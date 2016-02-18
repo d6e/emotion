37df4315f2b8f7a1edb408681f8c4a84617c0335
Danielle Jenkins <djenkins@splunk.com>
2016-02-17 19:54:55 -0800

    Add git url.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author_email="d@d6e.io",
     description="A simple emote tool for quickly fetching common emoticon/emoji.",
     install_requires=['pyperclip'],
-    url="",
+    url="https://github.com/d6e/emote",
     entry_points={
             "console_scripts": ["emote = emote:parse_cli"],
     }
